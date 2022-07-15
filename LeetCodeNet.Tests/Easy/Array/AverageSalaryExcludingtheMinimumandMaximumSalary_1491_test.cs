0fd3c30a33a16b12399edc4968e9641bcdf15f23
a.kulikov <kulikov.dev@gmail.com>
2022-07-15 15:35:59 +0500

    Problem 1491

---
+++ LeetCodeNet.Tests/Easy/Array/AverageSalaryExcludingtheMinimumandMaximumSalary_1491_test.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeNet.Easy.Array;
+
+namespace LeetCodeNet.Tests.Easy.Array
+{
+    public class AverageSalaryExcludingtheMinimumandMaximumSalary_1491_test
+    {
+        [Fact]
+        public void CheckLinq()
+        {
+            var solver = new AverageSalaryExcludingtheMinimumandMaximumSalary_1491();
+            //// Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+            /// Average salary excluding minimum and maximum salary is (2000 + 3000) / 2 = 2500
+            Assert.Equal(2500d, solver.AverageLinq(new int[] { 4000, 3000, 1000, 2000 }));
+            Assert.Equal(2000d, solver.AverageLinq(new int[] { 1000, 2000, 3000 }));
+            var salaries = new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 };
+            Assert.Equal(41111.111, Math.Round(solver.AverageLinq(salaries), 3));
+        }
+
+        [Fact]
+        public void CheckSorting()
+        {
+            var solver = new AverageSalaryExcludingtheMinimumandMaximumSalary_1491();
+            Assert.Equal(2500d, solver.AverageSorting(new int[] { 4000, 3000, 1000, 2000 }));
+            Assert.Equal(2000d, solver.AverageSorting(new int[] { 1000, 2000, 3000 }));
+            var salaries = new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 };
+            Assert.Equal(41111.111, Math.Round(solver.AverageSorting(salaries),3));
+        }
+
+        [Fact]
+        public void CheckIterating()
+        {
+            var solver = new AverageSalaryExcludingtheMinimumandMaximumSalary_1491();
+            Assert.Equal(2500d, solver.AverageIterating(new int[] { 4000, 3000, 1000, 2000 }));
+            Assert.Equal(2000d, solver.AverageIterating(new int[] { 1000, 2000, 3000 }));
+            var salaries = new int[] { 48000, 59000, 99000, 13000, 78000, 45000, 31000, 17000, 39000, 37000, 93000, 77000, 33000, 28000, 4000, 54000, 67000, 6000, 1000, 11000 };
+            Assert.Equal(41111.111, Math.Round(solver.AverageIterating(salaries), 3));
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Array/AverageSalaryExcludingtheMinimumandMaximumSalary_1491.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+    /// </summary>
+    public class AverageSalaryExcludingtheMinimumandMaximumSalary_1491
+    {
+        /// <summary>
+        /// Return the average salary of employees excluding the minimum and maximum salary
+        /// </summary>
+        /// <param name="salary"> Array of salaries </param>
+        /// <returns> Average salary </returns>
+        public double AverageLinq(int[] salary)
+        {
+            //// Not optimal approach, as we go through array for 3 times for each linq query.
+            var min = salary.Min();
+            var max = salary.Max();
+            var sum = salary.Sum();
+            var substractSum = sum - max - min;
+            //// Cast to double to prevent loss of precision.
+            return substractSum / (double)(salary.Length - 2);
+        }
+
+        /// <summary>
+        /// Return the average salary of employees excluding the minimum and maximum salary
+        /// </summary>
+        /// <param name="salary"> Array of salaries </param>
+        /// <returns> Average salary </returns>
+        public double AverageSorting(int[] salary)
+        {
+            //// The idea is to sort the array and then sum from 2 element to N-1 element
+            /// It's necessary to create a new variable, as the array was passed by reference.
+            /// It's not a good idea to change initial array in the real life.
+            var sortedSalary = salary.OrderBy(x => x).ToArray();
+
+            var sum = 0;
+            for (var i = 1; i < sortedSalary.Length - 1; ++i)
+            {
+                sum += sortedSalary[i];
+            }
+
+            return sum / (double)(sortedSalary.Length - 2);
+        }
+
+        /// <summary>
+        /// Return the average salary of employees excluding the minimum and maximum salary
+        /// </summary>
+        /// <param name="salary"> Array of salaries </param>
+        /// <returns> Average salary </returns>
+        public double AverageIterating(int[] salary)
+        {
+            //// The same approach as Linq, but we find here min/max through one pass.
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            var sum = 0;
+            for (var i = 0; i < salary.Length; ++i)
+            {
+                var currentSalary = salary[i];
+                min = Math.Min(min, currentSalary);
+                max = Math.Max(max, currentSalary);
+                sum += currentSalary;
+            }
+
+            return (sum - max - min) / (double)(salary.Length - 2);
+        }
+    }
+}
