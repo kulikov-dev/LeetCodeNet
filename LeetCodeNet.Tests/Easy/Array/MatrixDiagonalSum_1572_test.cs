3d739b4ec2831210514105898a32963300f91d5b
a.kulikov <kulikov.dev@gmail.com>
2022-07-15 17:59:34 +0500

    Problem #1572

---
+++ LeetCodeNet.Tests/Easy/Array/MatrixDiagonalSum_1572_test.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeNet.Easy.Array;
+
+namespace LeetCodeNet.Tests.Easy.Array
+{
+    public class MatrixDiagonalSum_1572_test
+    {
+        [Fact]
+        public void Check()
+        {
+            var solver = new MatrixDiagonalSum_1572();
+
+            //// Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+            /// Notice that element mat[1][1] = 5 is counted only once.
+            var input = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } };
+            var result = solver.DiagonalSum(input);
+            Assert.Equal(25, result);
+
+            input = new int[][] { new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 } };
+            result = solver.DiagonalSum(input);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void CheckOptimized()
+        {
+            var solver = new MatrixDiagonalSum_1572();
+
+            //// Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+            /// Notice that element mat[1][1] = 5 is counted only once.
+            var input = new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }, new int[] { 7, 8, 9 } };
+            var result = solver.DiagonalSumOptimized(input);
+            Assert.Equal(25, result);
+
+            input = new int[][] { new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 }, new int[] { 1, 1, 1, 1 } };
+            result = solver.DiagonalSumOptimized(input);
+            Assert.Equal(8, result);
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Array/MatrixDiagonalSum_1572.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/matrix-diagonal-sum/
+    /// </summary>
+    /// <remarks> Given a square matrix mat, return the sum of the matrix diagonals. </remarks>
+    public sealed class MatrixDiagonalSum_1572
+    {
+        /// <summary>
+        /// Take elements from left side and right side in one step. Keep in mind exception for odd length matrix (we don't need to sum a center element twice)
+        /// Kind of two pointers approach
+        /// </summary>
+        /// <param name="mat"> Source matrix </param>
+        /// <returns> Sum of diagonals </returns>
+        /// <remarks>
+        /// Time complexity: O(n), one pass
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int DiagonalSum(int[][] mat)
+        {
+            var result = 0;
+            var j = mat.Length - 1;
+            for (var i = 0; i < mat.Length; i++)
+            {
+                result += mat[i][i];        // Primary diagonals, where row == column
+                if (i != j)                 // Prevent adding center element for the odd matrix
+                {
+                    result += mat[i][j];    // Secondary diagonals, where row + column = n-1
+                }
+
+                --j;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// We can optimize the previous solution, by reducing if condition
+        /// </summary>
+        /// <param name="mat"> Source matrix </param>
+        /// <returns> Sum of diagonals </returns>
+        /// <remarks>
+        /// Time complexity: O(n), one pass
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int DiagonalSumOptimized(int[][] mat)
+        {
+            var result = 0;
+            var j = mat.Length - 1;
+            for (var i = 0; i < mat.Length; i++)
+            {
+                result += mat[i][i];        // Primary diagonals, where row == column
+                result += mat[i][j];    // Secondary diagonals, where row + column = n-1
+                --j;
+            }
+
+            //// So, check matrix for odd length in the end and remove one double added central element
+            var centerPos = mat.Length / 2;
+            result = mat.Length % 2 == 0 ? result : result - mat[centerPos][centerPos];
+            return result;
+        }
+    }
+}
