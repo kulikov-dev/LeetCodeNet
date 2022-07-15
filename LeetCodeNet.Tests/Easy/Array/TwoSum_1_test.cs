fb27b34956584932524e21a47c5a89419cf473fc
a.kulikov <kulikov.dev@gmail.com>
2022-07-15 14:27:16 +0500

    First commit. Problem #1

---
+++ LeetCodeNet.Tests/Easy/Array/TwoSum_1_test.cs
@@ -0,0 +1,23 @@
+using LeetCodeNet.Easy.Array;
+
+namespace LeetCodeNet.Tests.Easy.Array
+{
+    public class TwoSum_1_test
+    {
+        [Fact]
+        public void CheckMethod1()
+        {
+            var solver = new TwoSum_1();
+            var result = TwoSum_1.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+            //// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+            Assert.Equal(result, new int[] { 0, 1 });
+
+            result = TwoSum_1.TwoSum(new int[] { 3, 2, 4 }, 6);
+            Assert.Equal(result, new int[] { 1, 2 });
+
+            result = TwoSum_1.TwoSum(new int[] { 3, 3 }, 6);
+            Assert.Equal(result, new int[] { 0, 1 });
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Array/TwoSum_1.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/two-sum/
+    /// </summary>
+    public class TwoSum_1
+    {
+        /// <summary>
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// </summary>
+        /// <param name="nums"> List of nums </param>
+        /// <param name="target"> Value we need to find </param>
+        /// <returns> Indices of the two numbers </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(n)
+        /// </remarks>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            //// The idea is to use Hash for storing: difference between target number and current number for Key; and index as value
+            var result = new Dictionary<int, int>(nums.Length);
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                var diff = target - nums[i];
+                if (result.ContainsKey(diff))
+                {
+                    return new int[2] { result[diff], i };
+                }
+
+                result.Add(nums[i], i);
+            }
+
+            throw new ArgumentException("Input parameters not valid");
+        }
+    }
+}
