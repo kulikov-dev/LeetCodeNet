3128c5da4e93c6af9e104b328aaaacc656ecd1d6
a.kulikov <kulikov.dev@gmail.com>
2022-07-15 15:14:15 +0500

    Problem #125

---
+++ LeetCodeNet.Tests/Easy/Strings/ValidPalindrome_125_test.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeNet.Easy.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNet.Tests.Easy.Strings
+{
+    public class ValidPalindrome_125_test
+    {
+        [Fact]
+        public void CheckLinqMethod()
+        {
+            var solver = new ValidPalindrome_125();
+            //// Explanation: "amanaplanacanalpanama" is a palindrome.
+            Assert.True(solver.IsPalindromeLinq("A man, a plan, a canal: Panama"));
+            //// Explanation: "raceacar" is not a palindrome.
+            Assert.False(solver.IsPalindromeLinq("race a car"));
+            Assert.True(solver.IsPalindromeLinq("   "));
+        }
+
+        [Fact]
+        public void CheckPointersMethod()
+        {
+            var solver = new ValidPalindrome_125();
+            //// Explanation: "amanaplanacanalpanama" is a palindrome.
+            Assert.True(solver.IsPalindromePointers("A man, a plan, a canal: Panama"));
+            //// Explanation: "raceacar" is not a palindrome.
+            Assert.False(solver.IsPalindromePointers("race a car"));
+            Assert.True(solver.IsPalindromePointers("   "));
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Strings/ValidPalindrome_125.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodeNet.Easy.Strings
+{
+    /// <summary>
+    /// https://leetcode.com/problems/valid-palindrome/
+    /// </summary>
+    public class ValidPalindrome_125
+    {
+        /// <summary>
+        /// Given a string s, return true if it is a palindrome, or false otherwise.
+        /// </summary>
+        /// <param name="s"> String </param>
+        /// <returns> Flag if palindrome </returns>
+        public bool IsPalindromeLinq(string s)
+        {
+            //// Use LINQ approach to remove all not valid chars and reverse string. Then just compare initial and reversed strings 
+            var validString = new string(s.Where(x => char.IsLetterOrDigit(x)).ToArray());
+            var reversedString = new string(validString.Reverse().ToArray());
+            //// We use OrdinalIgnoreCase, because 'A' and 'a' are equals. According to Richter it's more prefferable than CurrentCultureIgnoreCase.
+            return validString.Equals(reversedString, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Given a string s, return true if it is a palindrome, or false otherwise.
+        /// </summary>
+        /// <param name="s"> String </param>
+        /// <returns> Flag if palindrome </returns>
+        public bool IsPalindromePointers(string s)
+        {
+            //// Use two-pointers approach to check each char step by step from left and right side.
+            var leftIndex = 0;
+            var rightIndex = s.Length - 1;
+            //// Instead of StringComparison we can just use ToUpper to put all chars in the same case.
+            s = s.ToUpper();
+            while (leftIndex < rightIndex)
+            {
+                while (leftIndex < s.Length && !char.IsLetterOrDigit(s[leftIndex]))
+                {
+                    ++leftIndex;
+                }
+
+                while (rightIndex > -1 && !char.IsLetterOrDigit(s[rightIndex]))
+                {
+                    --rightIndex;
+                }
+
+                if (leftIndex < rightIndex && s[leftIndex] != s[rightIndex])
+                {
+                    return false;
+                }
+
+                ++leftIndex;
+                --rightIndex;
+            }
+
+            return true;
+        }
+    }
+}
