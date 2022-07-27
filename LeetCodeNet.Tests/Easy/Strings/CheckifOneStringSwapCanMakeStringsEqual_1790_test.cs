98f1d98185ae58425cc7757118cf1cfcfe82ab16
a.kulikov <kulikov.dev@gmail.com>
2022-07-27 09:52:39 +0500

    Problem #1790

---
+++ LeetCodeNet.Tests/Easy/Strings/CheckifOneStringSwapCanMakeStringsEqual_1790_test.cs
@@ -0,0 +1,50 @@
+﻿using LeetCodeNet.Easy.Strings;
+using System.Collections;
+
+namespace LeetCodeNet.Tests.Easy.Strings
+{
+    public class CheckifOneStringSwapCanMakeStringsEqual_1790_test
+    {
+        [Theory, ClassData(typeof(CheckifOneStringSwapCanMakeStringsEqualTestData))]
+        public void Check(string inputData1, string inputData2, bool expected)
+        {
+            var solver = new CheckifOneStringSwapCanMakeStringsEqual_1790();
+            Assert.Equal(expected, solver.AreAlmostEqual(inputData1, inputData2));
+        }
+    }
+
+    public class CheckifOneStringSwapCanMakeStringsEqualTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //// Explanation: For example, swap the first character with the last character of s2 to make "bank".
+            yield return new object[]
+            {
+                "bank",
+                "kanb",
+                true
+            };
+
+            //// Explanation: It is impossible to make them equal with one string swap.
+            yield return new object[]
+            {
+                "attack",
+                "defend",
+                false
+            };
+
+            //// Explanation: The two strings are already equal, so no string swap operation is required.
+            yield return new object[]
+            {
+                "kelb",
+                "kelb",
+                true
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Strings/CheckifOneStringSwapCanMakeStringsEqual_1790.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCodeNet.Easy.Strings
+{
+    /// <summary>
+    /// https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
+    /// </summary>
+    /// <remarks>
+    /// You are given two strings s1 and s2 of equal length. A string swap is an operation where you choose two indices in a string (not necessarily different)
+    /// and swap the characters at these indices.
+    /// Return true if it is possible to make both strings equal by performing at most one string swap on exactly one of the strings.Otherwise, return false.
+    /// </remarks>
+    public class CheckifOneStringSwapCanMakeStringsEqual_1790
+    {
+        /// <summary>
+        /// Iterate through two strings and find all indexes of different chars. We met conditions, if we don't have difference or we have two different chars, which equals
+        /// </summary>
+        /// <param name="s1"> String 1 </param>
+        /// <param name="s2"> String 2 </param>
+        /// <returns> Flag, if possible to make both strings equal by performing at most one string swap </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            //// Store positions of diff characters
+            var diffCharPos1 = -1;
+            var diffcharPos2 = -1;
+            var diffCounts = 0;
+            for (var i = 0; i < s1.Length; ++i)
+            {
+                if (s1[i] != s2[i])
+                {
+                    ++diffCounts;
+                    if (diffCharPos1 == -1)
+                    {
+                        diffCharPos1 = i;
+                    }
+                    else
+                    {
+                        diffcharPos2 = i;
+                    }
+
+                    //// We can break iterations, if diff count is more than 2
+                    if (diffCounts > 2)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //// No difference or same characters are swapped
+            return diffCounts == 0 || s1[diffCharPos1] == s2[diffcharPos2] && s1[diffcharPos2] == s2[diffCharPos1];
+        }
+    }
+}
