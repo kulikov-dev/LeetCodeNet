5f941614f7046e67a77d9227530dc5f52a3796c2
kulikov-dev <kulikov.dev@gmail.com>
2022-08-09 23:41:32 +0500

    Problem #617

---
+++ LeetCodeNet.Tests/Easy/Trees/MergeTwoBinaryTrees_617_test.cs
@@ -0,0 +1,47 @@
+﻿using LeetCodeNet.DataStructs;
+using LeetCodeNet.Easy.Trees;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNet.Tests.Easy.Trees
+{
+    public class MergeTwoBinaryTrees_617_test
+    {
+        [Theory, ClassData(typeof(MergeTwoBinaryTreesTestData))]
+        public void CheckRecursive(TreeNode inputData1, TreeNode inputData2, TreeNode expected)
+        {
+            var solver = new MergeTwoBinaryTrees_617();
+            var result = solver.MergeTreesRecursive(inputData1, inputData2);
+            Assert.True(expected.Equals(result));
+        }
+    }
+
+    public class MergeTwoBinaryTreesTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new TreeNode(1, left: new TreeNode(3, left: new TreeNode(5)), right: new TreeNode(2)),
+                new TreeNode(2, left: new TreeNode(1, right: new TreeNode(4)), right: new TreeNode(3, right: new TreeNode(7))),
+                new TreeNode(3, left: new TreeNode(4, left: new TreeNode(5), new TreeNode(4)), right: new TreeNode(5, right: new TreeNode(7)))
+            };
+
+            yield return new object[]
+            {
+                new TreeNode(1),
+                new TreeNode(1, left:new TreeNode(2)),
+                new TreeNode(2, left: new TreeNode(2))
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Trees/MergeTwoBinaryTrees_617.cs
@@ -0,0 +1,45 @@
+﻿using LeetCodeNet.DataStructs;
+
+namespace LeetCodeNet.Easy.Trees
+{
+    /// <summary>
+    /// https://leetcode.com/problems/merge-two-binary-trees/
+    /// </summary>
+    /// <remarks>
+    /// Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. 
+    /// You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. 
+    /// Otherwise, the NOT null node will be used as the node of the new tree.
+    /// Return the merged tree.
+    /// </remarks>
+    public class MergeTwoBinaryTrees_617
+    {
+        /// <summary>
+        /// Another variant of BFS. Move level by level and change values
+        /// </summary>
+        /// <param name="root1"> Root 1 </param>
+        /// <param name="root2"> Root 2 </param>
+        /// <returns> A merged tree root </returns>
+        /// <remarks>
+        /// Time complexity: O(m)
+        /// Space complexity: O(m)
+        /// </remarks>
+        public TreeNode MergeTreesRecursive(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+            {
+                return null;
+            }
+
+            var value1 = root1?.val ?? 0;
+            var value2 = root2?.val ?? 0;
+            //// We don't change the source trees, as it is a bad practise
+            var newRoot = new TreeNode(value1 + value2)
+            {
+                left = MergeTreesRecursive(root1?.left, root2?.left),
+                right = MergeTreesRecursive(root1?.right, root2?.right)
+            };
+
+            return newRoot;
+        }
+    }
+}
