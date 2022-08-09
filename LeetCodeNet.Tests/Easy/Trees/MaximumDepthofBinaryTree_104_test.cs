72824de329442f9c9ea7bea60deeafca56a27740
kulikov-dev <kulikov.dev@gmail.com>
2022-08-09 22:33:03 +0500

    Problem #104

---
+++ LeetCodeNet.Tests/Easy/Trees/MaximumDepthofBinaryTree_104_test.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeNet.DataStructs;
+using LeetCodeNet.Easy.Trees;
+using System.Collections;
+
+namespace LeetCodeNet.Tests.Easy.Trees
+{
+    public class MaximumDepthofBinaryTree_104_test
+    {
+        [Theory, ClassData(typeof(MaximumDepthofBinaryTreeTestData))]
+        public void CheckRecursive(TreeNode inputData, int expected)
+        {
+            var solver = new MaximumDepthofBinaryTree_104();
+            var result = solver.MaxDepthRecursive(inputData);
+            Assert.True(expected.Equals(result));
+        }
+
+        [Theory, ClassData(typeof(MaximumDepthofBinaryTreeTestData))]
+        public void CheckIterative(TreeNode inputData, int expected)
+        {
+            var solver = new MaximumDepthofBinaryTree_104();
+            var result = solver.MaxDepthIterative(inputData);
+            Assert.True(expected.Equals(result));
+        }
+    }
+
+    public class MaximumDepthofBinaryTreeTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new TreeNode(3, left: new TreeNode(9), right: new TreeNode(20, left: new TreeNode(15), right: new TreeNode(9))),
+                3
+            };
+
+            yield return new object[]
+            {
+                new TreeNode(1, right: new TreeNode(3)),
+                2
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Trees/MaximumDepthofBinaryTree_104.cs
@@ -0,0 +1,75 @@
+﻿using LeetCodeNet.DataStructs;
+
+namespace LeetCodeNet.Easy.Trees
+{
+    /// <summary>
+    /// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    /// <remarks>
+    /// Given the root of a binary tree, return its maximum depth.
+    /// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+    /// </remarks>
+    public class MaximumDepthofBinaryTree_104
+    {
+        /// <summary>
+        /// Recursive variant of BFS
+        /// </summary>
+        /// <param name="root"> Root </param>
+        /// <returns> Maximum depth </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int MaxDepthRecursive(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(MaxDepthRecursive(root.left), MaxDepthIterative(root.right)) + 1;
+        }
+
+        /// <summary>
+        /// Iterative variant of BFS. Use queue to traverse through each level of a tree
+        /// </summary>
+        /// <param name="root"> Root </param>
+        /// <returns> Maximum depth </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(h)
+        /// </remarks>
+        public int MaxDepthIterative(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var queue = new Queue<TreeNode>();
+            var result = 0;
+            queue.Enqueue(root);
+            while (queue.Any())
+            {
+                ++result;
+                var length = queue.Count;
+                //// Iterate through nodes of a level
+                for (var i = 0; i < length; ++i)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
