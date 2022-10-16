ab9dcf054dccdb9c52c78b17f22947b51bac1cb0
kulikov-dev <kulikov.dev@gmail.com>
2022-10-16 21:10:53 +0600

    Problem 190

---
+++ LeetCodeNet.Tests/Easy/BitManipulation/ReverseBits_190_test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.BitManipulation;
+
+namespace LeetCodeNet.Tests.Easy.BitManipulation
+{
+    public sealed class ReverseBits_190_test
+    {
+        [Theory, ClassData(typeof(ReverseBitsTestData))]
+        public void CheckBit(uint inputData, uint expected)
+        {
+            var solver = new ReverseBits_190();
+            var result = solver.reverseBits(inputData);
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class ReverseBitsTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                43261596,
+                964176192
+            };
+
+            yield return new object[]
+            {
+                4294967293,
+                3221225471
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/BitManipulation/ReverseBits_190.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeNet.Easy.BitManipulation
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-bits/
+    /// </summary>
+    /// <remarks> Reverse bits of a given 32 bits unsigned integer. </remarks>
+    public sealed class ReverseBits_190
+    {
+        /// <summary>
+        /// Bit solution
+        /// </summary>
+        /// <param name="n"> Number </param>
+        /// <returns> Reversed number </returns>
+        /// <remarks>
+        /// Time complexity: O(1)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public uint reverseBits(uint n)
+        {
+            uint result = 0;
+
+            // We have 32 bit number, so to not to miss spaces - we will iterate through all digits
+            for (var i = 0; i < 32; ++i)
+            {
+                result <<= 1;
+                result |= (n & 1);
+                n >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
