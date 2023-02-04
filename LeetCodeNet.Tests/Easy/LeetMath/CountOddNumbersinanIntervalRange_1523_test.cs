1b1998ed63ff2d6ceac3434a8f424a6cffe6975a
kulikov-dev <kulikov.dev@gmail.com>
2023-02-04 21:00:20 +0300

    Problem #1523

---
+++ LeetCodeNet.Tests/Easy/LeetMath/CountOddNumbersinanIntervalRange_1523_test.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.LeetMath;
+
+namespace LeetCodeNet.Tests.Easy.LeetMath
+{
+    public sealed class CountOddNumbersinanIntervalRange_1523_test
+    {
+        [Theory, ClassData(typeof(CountOddNumbersinanIntervalRangeTestData))]
+        public void CheckSimple(int low, int high, int expected)
+        {
+            var solver = new CountOddNumbersinanIntervalRange_1523();
+            var result = solver.CountOdds(low,high);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class CountOddNumbersinanIntervalRangeTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                3, 7, 3
+            };
+
+            yield return new object[]
+            {
+                8, 10, 1
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/LeetMath/CountOddNumbersinanIntervalRange_1523.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeNet.Easy.LeetMath
+{
+    /// <summary>
+    /// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+    /// </summary>
+    /// <remarks>
+    /// Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+    /// </remarks>
+    public sealed class CountOddNumbersinanIntervalRange_1523
+    {
+        /// <summary>
+        /// In the array half of the values will always be odd values.
+        /// The only exception when both limits are even.
+        /// </summary>
+        /// <param name="low"> Low limit </param>
+        /// <param name="high"> High limit </param>
+        /// <returns> Odd amount </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int CountOdds(int low, int high)
+        {
+            var isEvenLow = low % 2 == 0;
+            var isEvenHigh = high % 2 == 0;
+
+            var halfValue = (high - low) / 2;
+
+            return halfValue + (isEvenLow && isEvenHigh ? 0 : 1);
+        }
+    }
+}
