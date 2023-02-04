074507b72b046b44d31d73702aab37ceee28b2bb
kulikov-dev <kulikov.dev@gmail.com>
2023-02-04 21:00:32 +0300

    Problem #13

---
+++ LeetCodeNet.Tests/Easy/LeetMath/RomanToInteger_13_test.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.LeetMath;
+
+namespace LeetCodeNet.Tests.Easy.LeetMath
+{
+    public sealed class RomanToInteger_13_test
+    {
+        [Theory, ClassData(typeof(RomanToIntegerTestData))]
+        public void CheckSimple(string inputData, int expected)
+        {
+            var solver = new RomanToInteger_13();
+            var result = solver.RomanToInt(inputData);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class RomanToIntegerTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                "III",
+                3
+            };
+
+            yield return new object[]
+            {
+                "IV",
+                4
+            };
+
+            //// Explanation: L = 50, V= 5, III = 3.
+            yield return new object[]
+            {
+                "LVIII",
+                58
+            };
+
+            //// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+            yield return new object[]
+            {
+                "MCMXCIV",
+                1994
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/LeetMath/RomanToInteger_13.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeNet.Easy.LeetMath
+{
+    /// <summary>
+    /// https://leetcode.com/problems/roman-to-integer/
+    /// </summary>
+    /// <remarks> Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    /// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+    /// </remarks>
+    public sealed class RomanToInteger_13
+    {
+        /// <summary>
+        /// Store dictionary with all possible roman values converted to arabian numbers.
+        /// We go from right to left and add values to result number.
+        /// If previous number is bigger than current - it's situation where we need to subtract value, like IV..
+        /// </summary>
+        /// <param name="inputStr"> Roman number </param>
+        /// <returns> Flag, if progression </returns>
+        /// <remarks>
+        /// Time complexity: O(1)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int RomanToInt(string inputStr)
+        {
+            var romanDict = new Dictionary<char, int> { { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 } };
+            var result = 0;
+            var prevValue = 0;
+
+            for (var i = inputStr.Length - 1; i >= 0; --i)
+            {
+                var currentChar = inputStr[i];
+                var currentValue = romanDict[currentChar];
+
+                if (currentValue < prevValue)
+                {
+                    result -= currentValue;
+                }
+                else
+                {
+                    result += currentValue;
+                }
+
+                prevValue = currentValue;
+            }
+
+            return result;
+        }
+    }
+}
