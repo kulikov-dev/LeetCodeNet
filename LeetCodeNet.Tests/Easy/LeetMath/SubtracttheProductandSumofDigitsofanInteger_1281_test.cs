c4bc5aec775db31c9a9577751568af5fc358c82f
kulikov-dev <kulikov.dev@gmail.com>
2023-02-07 16:00:08 +0300

    Problem #1281

---
+++ LeetCodeNet.Tests/Easy/LeetMath/SubtracttheProductandSumofDigitsofanInteger_1281_test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.LeetMath;
+
+namespace LeetCodeNet.Tests.Easy.LeetMath
+{
+    public sealed class SubtracttheProductandSumofDigitsofanInteger_1281_test
+    {
+        [Theory, ClassData(typeof(SubtracttheProductandSumofDigitsofanIntegerTestData))]
+        public void CheckSimple(int inputData, int expected)
+        {
+            var solver = new SubtracttheProductandSumofDigitsofanInteger_1281();
+            var result = solver.SubtractProductAndSum(inputData);
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class SubtracttheProductandSumofDigitsofanIntegerTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //// Product of digits = 2 * 3 * 4 = 24
+            /// Sum of digits = 2 + 3 + 4 = 9
+            /// Result = 24 - 9 = 15
+            yield return new object[]
+            {
+                234,
+                15
+            };
+
+            //// Product of digits = 4 * 4 * 2 * 1 = 32
+            /// Sum of digits = 4 + 4 + 2 + 1 = 11
+            /// Result = 32 - 11 = 21
+            yield return new object[]
+            {
+                4421,
+                21
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/LeetMath/SubtracttheProductandSumofDigitsofanInteger_1281.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeNet.Easy.LeetMath
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    /// </summary>
+    /// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+    public sealed class SubtracttheProductandSumofDigitsofanInteger_1281
+    {
+        /// <summary>
+        /// Keep in mind the '%' operator which return a modulo. This is enough to solve the task
+        /// </summary>
+        /// <param name="n"> Source number </param>
+        /// <returns> Diff between product and sum of source digits </returns>
+        /// <remarks>
+        /// Time complexity: O(1)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int SubtractProductAndSum(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            var prod = 1;
+            var sum = 0;
+            while (n != 0)
+            {
+                var temp = n % 10;
+                prod *= temp;
+                sum += temp;
+                n /= 10;
+            }
+
+            return prod - sum;
+        }
+    }
+}
