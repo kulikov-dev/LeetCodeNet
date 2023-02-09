cacfe5428e14961902f7f92ae5cf7a9f35f69222
kulikov-dev <kulikov.dev@gmail.com>
2023-02-09 15:07:22 +0300

    Problem #202

---
+++ LeetCodeNet.Tests/Easy/LeetMath/HappyNumber_202_test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.LeetMath;
+
+namespace LeetCodeNet.Tests.Easy.LeetMath
+{
+    public sealed class HappyNumber_202_test
+    {
+        [Theory, ClassData(typeof(HappyNumberTestData))]
+        public void CheckSimple(int inputData, bool expected)
+        {
+            var solver = new HappyNumber_202();
+            var result = solver.IsHappy(inputData);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class HappyNumberTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //// Explanation:
+            /// 12 + 92 = 82
+            /// 82 + 22 = 68
+            /// 62 + 82 = 100
+            /// 12 + 02 + 02 = 1
+            yield return new object[]
+            {
+                19,
+                true
+            };
+
+            yield return new object[]
+            {
+                2,
+                false
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/LeetMath/HappyNumber_202.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeNet.Easy.LeetMath
+{
+    /// <summary>
+    /// https://leetcode.com/problems/happy-number/
+    /// </summary>
+    /// <remarks>
+    ///Write an algorithm to determine if a number n is happy.
+    /// A happy number is a number defined by the following process:
+    /// Starting with any positive integer, replace the number by the sum of the squares of its digits.
+    /// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+    /// Those numbers for which this process ends in 1 are happy.
+    /// Return true if n is a happy number, and false if not.
+    /// </remarks>
+    public sealed class HappyNumber_202
+    {
+        /// <summary>
+        /// The trick here is how to detect when cycle is going to infinite loop. We will use simple hashset and if numbers start to duplicate - it shows us, that we are draw into infinite loop
+        /// </summary>
+        /// <param name="n"> Number </param>
+        /// <returns> Flag if happy </returns>
+        /// <remarks>
+        /// Time complexity: O(log(n))
+        /// Space complexity: O(log(n))
+        /// </remarks>
+        public bool IsHappy(int n)
+        {
+            var numbers = new HashSet<int>();
+            while (true)
+            {
+                var newNumber = 0;
+                while (n != 0)
+                {
+                    newNumber += (int)Math.Pow(n % 10, 2);
+                    n /= 10;
+                }
+
+                n = newNumber;
+                if (n == 1)
+                {
+                    return true;
+                }
+
+                if (numbers.Contains(n))
+                {
+                    return false;
+                }
+
+                numbers.Add(n);
+            }
+        }
+    }
+}
