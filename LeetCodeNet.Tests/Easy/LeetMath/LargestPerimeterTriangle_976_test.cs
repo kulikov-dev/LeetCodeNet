cc6f8a990a66db9c2706bfb2beb93560d1fb0ecc
kulikov-dev <kulikov.dev@gmail.com>
2023-02-11 21:11:33 +0300

    Problem #976

---
+++ LeetCodeNet.Tests/Easy/LeetMath/LargestPerimeterTriangle_976_test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.LeetMath;
+
+namespace LeetCodeNet.Tests.Easy.LeetMath
+{
+    public sealed class LargestPerimeterTriangle_976_test
+    {
+        [Theory, ClassData(typeof(LargestPerimeterTriangleTestData))]
+        public void CheckSimple(int[] inputData, int expected)
+        {
+            var solver = new LargestPerimeterTriangle_976();
+            var result = solver.LargestPerimeter(inputData);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class LargestPerimeterTriangleTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //// Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+            yield return new object[]
+            {
+                new int[] {2,1,2},
+                5
+            };
+
+            ////You cannot use the side lengths 1, 1, and 2 to form a triangle.
+            /// You cannot use the side lengths 1, 1, and 10 to form a triangle.
+            /// You cannot use the side lengths 1, 2, and 10 to form a triangle.
+            /// As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+            yield return new object[]
+            {
+                new int[] {1,2,1,10},
+                0
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/LeetMath/LargestPerimeterTriangle_976.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNet.Easy.LeetMath
+{
+    /// <summary>
+    /// https://leetcode.com/problems/largest-perimeter-triangle/description/
+    /// </summary>
+    /// <remarks>
+    /// Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths.
+    /// If it is impossible to form any triangle of a non-zero area, return 0.
+    /// </remarks>
+    public sealed class LargestPerimeterTriangle_976
+    {
+        /// <summary>
+        /// We need to calc the perimeter which is sum of three sides of the triangle. We also need to check, that it's a correct formed triangle (where side1 < side2 + side3).
+        /// To detect the maxium perimeter - it's a good idea to sort all sides by descending and then check for triangle exist.
+        /// </summary>
+        /// <param name="nums"> Possible triangle sides </param>
+        /// <returns> Largest perimeter </returns>
+        /// <remarks>
+        /// Time complexity: O(n+log(n))
+        /// Space complexity: O(n)
+        /// </remarks>
+        public int LargestPerimeter(int[] nums)
+        {
+            var sortedNums = nums.OrderByDescending(x => x).ToList();
+            for (var i = 2; i < sortedNums.Count; ++i)
+            {
+                if (sortedNums[i - 2] < sortedNums[i - 1] + sortedNums[i])
+                {
+                    return sortedNums[i] + sortedNums[i - 1] + sortedNums[i - 2];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
