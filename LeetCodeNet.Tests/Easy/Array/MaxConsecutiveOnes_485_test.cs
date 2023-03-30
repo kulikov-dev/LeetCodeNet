3b7cca7fe06ffbb928fd4b5f1666da2b3778665e
kulikov-dev <kulikov.dev@gmail.com>
2023-03-30 14:32:57 +0300

    Problem #485

---
+++ LeetCodeNet.Tests/Easy/Array/MaxConsecutiveOnes_485_test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.Array;
+
+namespace LeetCodeNet.Tests.Easy.Array
+{
+    public sealed class MaxConsecutiveOnes_485_test
+    {
+        [Theory, ClassData(typeof(MaxConsecutiveOnesTestData))]
+        public void CheckSimple(int[] inputData, int expected)
+        {
+            var solver = new MaxConsecutiveOnes_485();
+            var result = solver.FindMaxConsecutiveOnesSimple(inputData);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory, ClassData(typeof(MaxConsecutiveOnesTestData))]
+        public void CheckShort(int[] inputData, int expected)
+        {
+            var solver = new MaxConsecutiveOnes_485();
+            var result = solver.FindMaxConsecutiveOnesShort(inputData);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class MaxConsecutiveOnesTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new []{1,1,0,1,1,1},
+                3
+            };
+
+            yield return new object[]
+            {
+                new []{1,0,1,1,0,1},
+                2
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Array/MaxConsecutiveOnes_485.cs
@@ -0,0 +1,67 @@
+﻿namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/max-consecutive-ones/
+    /// </summary>
+    /// <remarks>
+    /// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+    /// </remarks>
+    internal sealed class MaxConsecutiveOnes_485
+    {
+        /// <summary>
+        /// Use counter to save previous consecutive 1's number and iterate through the nums array.
+        /// Reset the current counter if nums[i] != 1
+        /// </summary>
+        /// <param name="nums"> Input </param>
+        /// <returns> Max consecutive ones </returns>
+        /// <remarks>
+        /// Time complexity: O(N)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int FindMaxConsecutiveOnesSimple(int[] nums)
+        {
+            var counter = 0;
+            var maxConsecutive = 0;
+
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                if (nums[i] == 1)
+                {
+                    ++counter;
+                }
+                else if (counter > 0)
+                {
+                    maxConsecutive = Math.Max(maxConsecutive, counter);
+                    counter = 0;
+                }
+            }
+
+            //// Just to check the situation, where the last values are 1's.
+            return Math.Max(maxConsecutive, counter);
+        }
+
+        /// <summary>
+        /// The same approach but with syntax sugar.
+        /// </summary>
+        /// <param name="nums"> Input </param>
+        /// <returns> Max consecutive ones </returns>
+        /// <remarks>
+        /// Time complexity: O(N)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int FindMaxConsecutiveOnesShort(int[] nums)
+        {
+            var counter = 0;
+            var maxConsecutive = 0;
+
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                counter = nums[i] == 0 ? 0 : counter + 1;
+                maxConsecutive = Math.Max(maxConsecutive, counter);
+            }
+
+            //// Just to check the situation, where the last values are 1's.
+            return Math.Max(maxConsecutive, counter);
+        }
+    }
+}
