9c87e88ef0e4dea3aaaa62ac6d93073494094eef
kulikov-dev <kulikov.dev@gmail.com>
2023-03-31 13:40:51 +0300

    Problem #1295

---
+++ LeetCodeNet.Tests/Easy/Array/FindNumberswithEvenNumberofDigits_1295_test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using LeetCodeNet.Easy.Array;
+
+namespace LeetCodeNet.Tests.Easy.Array
+{
+    public sealed class FindNumberswithEvenNumberofDigits_1295_test
+    {
+        [Theory, ClassData(typeof(FindNumberswithEvenNumberofDigitsTestData))]
+        public void Check(int[] inputData, int expected)
+        {
+            var solver = new FindNumberswithEvenNumberofDigits_1295();
+            var result = solver.FindNumbers(inputData);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public sealed class FindNumberswithEvenNumberofDigitsTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new []{12,345,2,6,7896},
+                2
+            };
+
+            yield return new object[]
+            {
+                new []{555,901,482,1771},
+                1
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Easy/Array/FindNumberswithEvenNumberofDigits_1295.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+    /// </summary>
+    /// <remarks> Given an array nums of integers, return how many of them contain an even number of digits. </remarks>
+    internal sealed class FindNumberswithEvenNumberofDigits_1295
+    {
+        /// <summary>
+        /// The trick here is to understand how to count the digits. The easiest way is simple divide the number to 10 until it will become 0, like:
+        /// Input: 512;
+        /// Step1: 512 / 10 = 51, result = 1
+        /// Step2: 51 / 10 = 5, result = 2
+        /// Step3: 5 / 10 = 0, result 3
+        /// Than, we just check if it is even using '%' operator.
+        /// </summary>
+        /// <param name="nums"> Input array </param>
+        /// <returns> Numbers count with even number of digits </returns>
+        /// <remarks>
+        /// Time complexity: O(N)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int FindNumbers(int[] nums)
+        {
+            var total = 0;
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                var digitsCount = CountDigits(nums[i]);
+
+                total += digitsCount % 2 == 0 ? 1 : 0;
+            }
+
+            return total;
+        }
+
+        /// <summary>
+        /// Help function to calc number of digits
+        /// </summary>
+        /// <param name="num"> Number </param>
+        /// <returns> Number of digits in the number </returns>
+        private int CountDigits(int num)
+        {
+            var counter = 0;
+
+            while (num != 0)
+            {
+                ++counter;
+                num /= 10;
+            }
+
+            return counter;
+        }
+    }
+}
