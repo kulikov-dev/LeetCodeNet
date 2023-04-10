b68f5f470833f4d140a530a588509d97ba8f4f2d
kulikov-dev <kulikov.dev@gmail.com>
2023-04-10 11:13:17 +0300

    Problem #102

---
+++ LeetCodeNet.Tests/Medium/Trees/BinaryTreeLevelOrderTraversal_102_test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using LeetCodeNet.DataStructs;
+using LeetCodeNet.Medium.Trees;
+
+namespace LeetCodeNet.Tests.Medium.Trees
+{
+    public sealed class BinaryTreeLevelOrderTraversal_102_test
+    {
+        [Theory, ClassData(typeof(BinaryTreeLevelOrderTraversalTestData))]
+        public void Check(TreeNode input, List<List<int>> expected)
+        {
+            var solver = new BinaryTreeLevelOrderTraversal_102();
+            var result = solver.LevelOrder(input);
+
+            Assert.Equal(expected.Count, result.Count);
+
+            for (var i = 0; i < expected.Count; i++)
+            {
+                Assert.True(expected[i].SequenceEqual(result[i]));
+            }
+        }
+    }
+
+    public sealed class BinaryTreeLevelOrderTraversalTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))),
+                new List<List<int>> {new() {3}, new() {9, 20}, new() {15,7}}
+            };
+
+            yield return new object[]
+            {
+                new TreeNode(1),
+                new List<List<int>> {new() {1}}
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LeetCodeNet/Medium/Trees/BinaryTreeLevelOrderTraversal_102.cs
@@ -0,0 +1,56 @@
+﻿using LeetCodeNet.DataStructs;
+
+namespace LeetCodeNet.Medium.Trees
+{
+    /// <summary>
+    /// https://leetcode.com/problems/binary-tree-level-order-traversal
+    /// </summary>
+    /// <remarks>
+    /// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+    /// </remarks>
+    internal sealed class BinaryTreeLevelOrderTraversal_102
+    {
+        /// <summary>
+        /// Use queue to process Breadth-First search.
+        /// </summary>
+        /// <param name="root"> Root </param>
+        /// <returns> Tree values by levels </returns>
+        /// <remarks>
+        /// Time complexity: O(N)
+        /// Space complexity: O(N)
+        /// </remarks>
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<List<int>>();
+            var queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var count = queue.Count;
+                var levelList = new List<int>();
+
+                for (var i = 0; i < count; ++i)         //// Queue allows us to move through all nodes on the same level
+                {
+                    var node = queue.Dequeue();
+
+                    if (node == null)
+                    {
+                        continue;
+                    }
+
+                    levelList.Add(node.val);
+                    queue.Enqueue(node.left);
+                    queue.Enqueue(node.right);
+                }
+
+                if (levelList.Count > 0)            //// Need to prevent adding the last level with all null nodes.
+                {
+                    result.Add(levelList);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
