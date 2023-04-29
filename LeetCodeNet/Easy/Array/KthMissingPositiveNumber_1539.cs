f7963576605e0c3408c026c300196b5691709250
kulikov-dev <kulikov.dev@gmail.com>
2023-04-29 23:12:58 +0300

    Problem #1539

---
+++ LeetCodeNet/Easy/Array/KthMissingPositiveNumber_1539.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeNet.Easy.Array
+{
+    /// <summary>
+    /// https://leetcode.com/problems/kth-missing-positive-number
+    /// </summary>
+    /// <remarks>
+    /// Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+    /// Return the kth positive integer that is missing from this array.
+    /// </remarks>
+    internal sealed class KthMissingPositiveNumber_1539
+    {
+        /// <summary>
+        /// Simple and elegant. Keep pointer to the array and calc missing numbers
+        /// </summary>
+        /// <param name="arr"> Input array </param>
+        /// <param name="k"> Kth missing </param>
+        /// <returns> Kth missing number </returns>
+        /// <remarks>
+        /// Time complexity: O(n)
+        /// Space complexity: O(1)
+        /// </remarks>
+        public int FindKthPositive(int[] arr, int k)
+        {
+            var missingNumbers = 0;
+            var arrIndex = 0;
+
+            for (var i = 1; i <= arr.Length +k + 1; ++i)
+            {
+                if (arrIndex < arr.Length && arr[arrIndex] == i)
+                {
+                    ++arrIndex;
+                }
+                else
+                {
+                    ++missingNumbers;
+                    if (missingNumbers == k)
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -32,6 +32,7 @@ All problems are grouped by patterns, so you can train in solving of specific ty
 * #1431: Kids With the Greatest Number of Candies (Array)
 * #1502: Can Make Arithmetic Progression From Sequence (Math, Array)
 * #1523: Count Odd Numbers in an Interval Range (Array)
+* #1539^ Kth missing positive number (Array)
 * #1572: Matrix Diagonal Sum (Array)
 * #1588: Sum of All Odd Length Subarrays (Array)
 * #1603: Design Parking System (Design)
